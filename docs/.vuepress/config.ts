0323885367ecbba335c6504966703c4352dd3d98
shi-zhong <1393652370@qq.com>
2023-08-06 20:59:53 +0800

    fix

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -9,6 +9,7 @@ export default defineUserConfig({
   lang: 'zh-CN',
   title: '你好， VuePress ！',
   description: '这是我的第一个 VuePress 站点',
+  base: '/blog/',
   alias: {
     '@@': path.resolve(__dir, '../../components')
   },
